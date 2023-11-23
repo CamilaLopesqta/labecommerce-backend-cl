b9eb6fd61ccbce6b4a720b8df3a4e7187b5fb088
Camila Lopes <camilalopesqta@gmail.com>
2023-11-23 20:42:27 -0300

    Exercícios de tipagem do TS

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+//console.log("Aplicativo funcionando com sucesso!!!");
+console.log(database_1.users);
+console.log(database_1.products);
