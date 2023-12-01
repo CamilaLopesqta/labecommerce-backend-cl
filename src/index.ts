60a2c5ad016105b540f59d30fb5f855d5741880d
Camila Lopes <camilalopesqta@gmail.com>
2023-12-01 16:53:03 -0300

    Exercícios de Typescript II

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import {users, products} from './database';
+import { log } from 'console';
+import {users, products, createUser, getAllUsers, createProduct, getAllProducts, searchProductsByName} from './database';
 
 //console.log("Aplicativo funcionando com sucesso!!!");
+//console.log(users);
+//console.log(products);
 
-console.log(users);
-console.log(products);
+//console.log(createUser("u003", "Astrodev", "astrodev@email.com", 
+//"astrodev99"))
+
+//console.log(getAllUsers());
+
+//console.log(createProduct("prod003", "SSD gamer", 349.99, "Acelere seu sistema com velocidades incríveis de leitura e gravação.", "https://images.unsplash.com/photo"));
+
+//console.log(getAllProducts());
+
+console.log(searchProductsByName("gamer"));
